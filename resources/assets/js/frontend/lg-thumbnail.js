c6a6af058387550732758538b4fa2cf43550b577
diana-grandwp <dianamn11@gmail.com>
2017-10-11 18:56:18 +0400

    thumb bug fix and other changes

--- resources/assets/js/frontend/lg-thumbnail.js
+++ resources/assets/js/frontend/lg-thumbnail.js
@@ -222,10 +222,7 @@
         $thumb.on('click.lg touchend.lg', function() {
             var _$this = $(this);
             setTimeout(function() {
-
-                // In IE9 and bellow touch does not support
-                // Go to slide if browser does not support css transitions
-                if ((_this.thumbClickable && !_this.core.lgBusy) || !_this.core.doCss()) {
+                if ((!_this.core.lgBusy) || !_this.core.doCss()) {
                     _this.core.index = _$this.index();
                     _this.core.slide(_this.core.index, false, true, false);
                 }
